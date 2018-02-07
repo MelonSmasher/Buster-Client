5a822f9224b3ef5488317fb26544de9bec2af771
Alex Markessinis <markea125@gmail.com>
2018-02-06 23:52:38 -0500

    created buster client

---
+++ src/Client.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: melon
+ * Date: 2/6/18
+ * Time: 11:30 PM
+ */
+
+use GuzzleHttp\Client as HttpClient;
+
+class Client
+{
+
+    protected $headers = [];
+
+    protected $httpClient;
+
+    protected $url = '';
+
+    /**
+     * BusterClient constructor.
+     * @param string $key
+     * @param string $host
+     * @param int $port
+     * @param bool $useHttps
+     */
+    public function __construct($key, $host, $port = 80, $useHttps = false)
+    {
+        $this->url = implode('', [($useHttps) ? 'https' : 'http', '://', $host, ':', strval($port), '/api/bust']);
+        $this->headers = ['x-authorization' => $key, 'Accept' => '*/*', 'Cache-Control' => 'no-cache'];
+        $this->httpClient = new HttpClient();
+    }
+
+    /**
+     * @param $pathToPurge
+     * @param $schemeId
+     * @param string $clientUserName
+     * @return \GuzzleHttp\Message\Response
+     */
+    public function bust($pathToPurge, $schemeId, $clientUserName = '')
+    {
+        $response = $this->httpClient->request(
+            'POST',
+            $this->url,
+            [
+                'debug' => false,
+                'http_errors' => false,
+                'form_params' => [
+                    'path' => $pathToPurge,
+                    'scheme_id' => $schemeId,
+                    'client_username' => $clientUserName
+                ]
+            ]
+        );
+
+        return $response;
+    }
+
+}
